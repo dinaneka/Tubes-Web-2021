0d70356b7b26b2a59c9e0cf9cc8b4a9500e14c6c
dinaneka <76057798+dinaneka@users.noreply.github.com>
2021-06-05 15:55:07 +0800

    PEMBAYARAN FIX DENGAN MIDTRANS

--- pembayaran.php
+++ pembayaran.php
@@ -1,11 +1,13 @@
+<script type="text/javascript" src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key="SB-Mid-client-c4wH5JnmoxeAQqF1"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <section class="payment-submit">
     <div class="payment-box">
-        <h3>RINCIAN</h3>
+        <h3>RINCIAN PEMBAYARAN</h3>
         <div class="foto-dokter">
             <img src=<?php echo base_url('Asset/img/81366-middle.png') ?> alt="Dokter">
             <div class="keterangan-dokter">
-                <p class="nama-dokter">Dr. Johny Sins</p>
-                <p class="jenis-dokter">Fake Doctor</p>
+                <p class="nama-dokter">Dr. Dinan Eka</p>
+                <p class="jenis-dokter">Spesialis Jantung</p>
             </div>
         </div>
         <div class="payment-desc">
@@ -19,8 +21,61 @@
                 <p>Rp. 20.000</p>
             </div>
         </div>
-        <a href="#" class="submit-payment"><button>Pilih Metode Pembayaran</button></a>
+        <div class="container">
+            <form id="payment-form" method="post" action="<?= site_url() ?>/snap/finish">
+                <input type="hidden" name="result_type" id="result-type" value="">
+                <input type="hidden" name="result_data" id="result-data" value="">
+                <a href="#" id="pay-button" class="submit-payment"><button>Lanjut ke Pembayaran</button></a>
+            </form>
+        </div>
     </div>
+    <script type="text/javascript">
+        $('#pay-button').click(function(event) {
+            event.preventDefault();
+            $(this).attr("disabled", "disabled");
+
+            $.ajax({
+                url: '<?= site_url() ?>/snap/token',
+                cache: false,
+
+                success: function(data) {
+                    //location = data;
+
+                    console.log('token = ' + data);
+
+                    var resultType = document.getElementById('result-type');
+                    var resultData = document.getElementById('result-data');
+
+                    function changeResult(type, data) {
+                        $("#result-type").val(type);
+                        $("#result-data").val(JSON.stringify(data));
+                        //resultType.innerHTML = type;
+                        //resultData.innerHTML = JSON.stringify(data);
+                    }
+
+                    snap.pay(data, {
+
+                        onSuccess: function(result) {
+                            changeResult('success', result);
+                            console.log(result.status_message);
+                            console.log(result);
+                            $("#payment-form").submit();
+                        },
+                        onPending: function(result) {
+                            changeResult('pending', result);
+                            console.log(result.status_message);
+                            $("#payment-form").submit();
+                        },
+                        onError: function(result) {
+                            changeResult('error', result);
+                            console.log(result.status_message);
+                            $("#payment-form").submit();
+                        }
+                    });
+                }
+            });
+        });
+    </script>
 </section>
 
 <section class="left-description">
@@ -28,7 +83,7 @@
         <h3>Konsultasi dengan dokter yang terpercaya</h3>
         <p>Konsultasi kesehatan membuat anda semakin banyak masalah</p>
         <div class="img-box">
-            <img src="<?php echo base_url('Asset/img/81366-middle.png') ?>" alt="">
+            <img src="<?php echo base_url('Asset/img/hospital.png') ?>" alt="">
         </div>
         <p>Manfaat berkonsultasi di web kami :</p>
         <div class="list-manfaat">
